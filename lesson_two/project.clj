0c0d8e87924fba005842c834ff284fc86d58a08d
南山竹 <junjiemars@gmail.com>
2017-03-14 21:49:57 +0800

    figwheel upgrade to 0.5.9

--- lesson_two/project.clj
+++ lesson_two/project.clj
@@ -1,7 +1,7 @@
 (defproject arlo "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]]
-  :plugins [[lein-figwheel "0.5.8"]]
+  :plugins [[lein-figwheel "0.5.9"]]
   :clean-targets ^{:protect false} ["resources/public/js"
                                     "resources/public/js/arlo.js"
                                     :target-path]
